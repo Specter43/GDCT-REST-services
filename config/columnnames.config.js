f4e3a41ad78267c3d2c725054c2021a0f45ebbbc
yangzhey <zheyu.yang@mail.utoronto.ca>
2020-12-17 05:05:44 -0500

    new gitignore

--- config/columnnames.config.js
+++ config/columnnames.config.js
@@ -1,4 +1,7 @@
 module.exports = {
     //'mongoUrl':'mongodb://localhost:27017/conFusion'
     'mongoUrl': 'mongodb+srv://lime:Password123!@wp-data-g0lqo.mongodb.net/DCT?retryWrites=true&amp;w=majority'
+    //'mongoUrl': 'mongodb+srv://lime:Password123!@wp-data-g0lqo.mongodb.net/DCT_TonyP?retryWrites=true&w=majority'
+    //'mongoUrl': 'mongodb+srv://lime:Password123!@wp-data-g0lqo.mongodb.net/DCT_Jie?retryWrites=true&amp;w=majority'
+    //'mongoUrl': 'mongodb+srv://lime:Password123!@wp-data-g0lqo.mongodb.net/DCT_Online_SD?retryWrites=true&amp;w=majority'
 }
