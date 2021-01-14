aa5bbfa7410aabd2d95c96a2134c63e5e798c249
Tony Park <tpark43@gmail.com>
2021-01-14 11:32:16 -0500

    modified response

--- app/controllers/templateTypes.controller.js
+++ app/controllers/templateTypes.controller.js
@@ -2,6 +2,12 @@ const TemplateTypeService = require('../service/templateTypes.service');
 
 exports.find = (req, res) => {
     TemplateTypeService.find().then(templateTypes => {
+        templateTypes = templateTypes.map(type => ({
+            [type._id]: {
+                name: type.name,
+                description: type.description
+            }
+        }))
         res.status(200).json({
             status: 'success',
             data: templateTypes,
